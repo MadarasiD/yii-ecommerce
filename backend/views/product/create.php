f38605d27311b5ebe5c80b612d813195fca4b419
MadarasiD <madarka0913@gmail.com>
2025-11-14 19:32:25 +0100

    Install yii2 ckeditor

--- backend/views/product/create.php
+++ backend/views/product/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\Products $model */
 
-$this->title = 'Create Products';
+$this->title = 'Termék létrehozása';
 $this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
